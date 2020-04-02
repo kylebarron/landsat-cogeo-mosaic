bce0836e41ed7481fd883c3d8b988791cefdc571
Kyle Barron <barronk@mit.edu>
2020-04-02 16:28:39 -0600

    Move mosaicjson creation into mosaic.py

--- landsat_cogeo_mosaic/mosaic.py
+++ landsat_cogeo_mosaic/mosaic.py
@@ -32,60 +32,98 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import json
 import os
+import sys
 import urllib
 from datetime import datetime
-from typing import Any, List, Tuple
+from typing import Any, Dict, List, Tuple
 
-from .stac import stac_to_mosaicJSON
+import mercantile
+from shapely.geometry import box, shape
+from supermercado import burntiles
+
+from .util import bbox_to_geojson
 
 
-def create_mosaic(
-        bounds: List[float],
-        min_cloud: float,
-        max_cloud: float,
-        min_date: str = "2013-01-01",
-        max_date: str = "2019-12-01",
-        min_zoom: int = 7,
-        max_zoom: int = 12,
+def features_to_mosaicJSON(
+        features: List,
+        minzoom: int = 7,
+        maxzoom: int = 12,
         optimized_selection: bool = True,
         maximum_items_per_tile: int = 20,
-        stac_collection_limit: int = None,
-        seasons: List[str] = None):
-    """Create mosaic"""
-    if seasons is None:
-        seasons = ["spring", "summer", "autumn", "winter"]
+        seasons: Tuple = ["spring", "summer", "autumn", "winter"],
+) -> Dict:
+    """
+    Create a mosaicJSON from a stac request.
 
-    start = datetime.strptime(min_date,
-                              "%Y-%m-%d").strftime("%Y-%m-%dT00:00:00Z")
-    end = datetime.strptime(max_date, "%Y-%m-%d").strftime("%Y-%m-%dT23:59:59Z")
+    Attributes
+    ----------
+    features : list
+        sat-api features.
+    minzoom : int, optional, (default: 7)
+        Mosaic Min Zoom.
+    maxzoom : int, optional (default: 12)
+        Mosaic Max Zoom.
+    optimized_selection : bool, optional (default: true)
+        Limit one Path-Row scene per quadkey.
+    maximum_items_per_tile : int, optional (default: 20)
+        Limit number of scene per quadkey. Use 0 to use all items.
 
-    query = {
-        "bbox": bounds,
-        "time": f"{start}/{end}",
-        "query": {
-            "eo:sun_elevation": {
-                "gt": 0},
-            "landsat:tier": {
-                "eq": "T1"},
-            "collection": {
-                "eq": "landsat-8-l1"},
-            "eo:cloud_cover": {
-                "gte": min_cloud,
-                "lt": max_cloud},
-            "eo:platform": {
-                "eq": "landsat-8"}, },
-        "sort": [{
-            "field": "eo:cloud_cover",
-            "direction": "asc"}], }
+    Returns
+    -------
+    out : dict
+        MosaicJSON definition.
 
-    mosaic_definition = stac_to_mosaicJSON(
-        query,
-        minzoom=min_zoom,
-        maxzoom=max_zoom,
-        optimized_selection=optimized_selection,
-        maximum_items_per_tile=maximum_items_per_tile,
-        stac_collection_limit=stac_collection_limit,
-        seasons=seasons,
+    """
+    if optimized_selection:
+        dataset = []
+        prs = []
+        for item in features:
+            pr = item["properties"]["eo:column"] + "-" + item["properties"][
+                "eo:row"]
+            if pr not in prs:
+                prs.append(pr)
+                dataset.append(item)
+    else:
+        dataset = features
+
+    if query.get("bbox"):
+        bounds = query["bbox"]
+    else:
+        bounds = burntiles.find_extrema(dataset)
+
+    for i in range(len(dataset)):
+        dataset[i]["geometry"] = shape(dataset[i]["geometry"])
+
+    tiles = burntiles.burn([bbox_to_geojson(bounds)], minzoom)
+    tiles = list(set(["{2}-{0}-{1}".format(*tile.tolist()) for tile in tiles]))
+
+    print(f"Number tiles: {len(tiles)}", file=sys.stderr)
+
+    mosaic_definition = dict(
+        mosaicjson="0.0.1",
+        minzoom=minzoom,
+        maxzoom=maxzoom,
+        bounds=bounds,
+        center=[(bounds[0] + bounds[2]) / 2, (bounds[1] + bounds[3]) / 2,
+                minzoom],
+        tiles={},
     )
+
+    for tile in tiles:
+        z, x, y = list(map(int, tile.split("-")))
+        tile = mercantile.Tile(x=x, y=y, z=z)
+        quadkey = mercantile.quadkey(*tile)
+        geometry = box(*mercantile.bounds(tile))
+        intersect_dataset = list(
+            filter(lambda x: geometry.intersects(x["geometry"]), dataset))
+        if len(intersect_dataset):
+            # We limit the item per quadkey to 20
+            if maximum_items_per_tile:
+                intersect_dataset = intersect_dataset[0:maximum_items_per_tile]
+
+            mosaic_definition["tiles"][quadkey] = [
+                scene["properties"]["landsat:product_id"]
+                for scene in intersect_dataset
+            ]
 
     return mosaic_definition
--- landsat_cogeo_mosaic/stac.py
+++ landsat_cogeo_mosaic/stac.py
@@ -32,17 +32,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import itertools
 import json
-import os
 import sys
 from datetime import datetime
-from typing import Dict, Tuple
 
-import mercantile
 import requests
-from shapely.geometry import box, shape
-from supermercado import burntiles
-
-from .util import bbox_to_geojson
 
 
 def _get_season(date, lat=0):
@@ -59,118 +52,12 @@ def _get_season(date, lat=0):
     return season_names[idx]
 
 
-def stac_to_mosaicJSON(
-        query: Dict,
-        minzoom: int = 7,
-        maxzoom: int = 12,
-        optimized_selection: bool = True,
-        maximum_items_per_tile: int = 20,
-        stac_collection_limit: int = None,
-        seasons: Tuple = ["spring", "summer", "autumn", "winter"],
-        stac_url: str = os.environ.get(
-            "SATAPI_URL", "https://sat-api.developmentseed.org"),
-) -> Dict:
-    """
-    Create a mosaicJSON from a stac request.
-
-    Attributes
-    ----------
-    query : dict
-        sat-api query.
-    minzoom : int, optional, (default: 7)
-        Mosaic Min Zoom.
-    maxzoom : int, optional (default: 12)
-        Mosaic Max Zoom.
-    optimized_selection : bool, optional (default: true)
-        Limit one Path-Row scene per quadkey.
-    maximum_items_per_tile : int, optional (default: 20)
-        Limit number of scene per quadkey. Use 0 to use all items.
-    stac_collection_limit : int, optional (default: None)
-        Limits the number of items returned by sat-api
-    stac_url : str, optional (default: from ENV)
-
-    Returns
-    -------
-    out : dict
-        MosaicJSON definition.
-
-    """
-    if stac_collection_limit:
-        query.update(limit=stac_collection_limit)
-
-    features = fetch_sat_api(query)
-    if not features:
-        raise Exception(f"No asset found for query '{json.dumps(query)}'")
-
-    print(f"Found: {len(features)} scenes", file=sys.stderr)
-
-    features = list(
-        filter(
-            lambda x: _get_season(
-                x["properties"]["datetime"], max(x["bbox"][1], x["bbox"][3])) in
-            seasons,
-            features,
-        ))
-
-    if optimized_selection:
-        dataset = []
-        prs = []
-        for item in features:
-            pr = item["properties"]["eo:column"] + "-" + item["properties"][
-                "eo:row"]
-            if pr not in prs:
-                prs.append(pr)
-                dataset.append(item)
-    else:
-        dataset = features
-
-    if query.get("bbox"):
-        bounds = query["bbox"]
-    else:
-        bounds = burntiles.find_extrema(dataset)
-
-    for i in range(len(dataset)):
-        dataset[i]["geometry"] = shape(dataset[i]["geometry"])
-
-    tiles = burntiles.burn([bbox_to_geojson(bounds)], minzoom)
-    tiles = list(set(["{2}-{0}-{1}".format(*tile.tolist()) for tile in tiles]))
-
-    print(f"Number tiles: {len(tiles)}", file=sys.stderr)
-
-    mosaic_definition = dict(
-        mosaicjson="0.0.1",
-        minzoom=minzoom,
-        maxzoom=maxzoom,
-        bounds=bounds,
-        center=[(bounds[0] + bounds[2]) / 2, (bounds[1] + bounds[3]) / 2,
-                minzoom],
-        tiles={},
-    )
-
-    for tile in tiles:
-        z, x, y = list(map(int, tile.split("-")))
-        tile = mercantile.Tile(x=x, y=y, z=z)
-        quadkey = mercantile.quadkey(*tile)
-        geometry = box(*mercantile.bounds(tile))
-        intersect_dataset = list(
-            filter(lambda x: geometry.intersects(x["geometry"]), dataset))
-        if len(intersect_dataset):
-            # We limit the item per quadkey to 20
-            if maximum_items_per_tile:
-                intersect_dataset = intersect_dataset[0:maximum_items_per_tile]
-
-            mosaic_definition["tiles"][quadkey] = [
-                scene["properties"]["landsat:product_id"]
-                for scene in intersect_dataset]
-
-    return mosaic_definition
-
-
 def fetch_sat_api(query, stac_url: str = "https://sat-api.developmentseed.org"):
     headers = {
         "Content-Type": "application/json",
         "Accept-Encoding": "gzip",
-        "Accept": "application/geo+json", }
+        "Accept": "application/geo+json",
+    }
 
     url = f"{stac_url}/stac/search"
     data = requests.post(url, headers=headers, json=query).json()
