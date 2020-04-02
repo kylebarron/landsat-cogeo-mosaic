00b2ec33673103861e13ff31bfeaf70c003232b0
Kyle Barron <barronk@mit.edu>
2020-04-02 16:06:59 -0600

    Split CLI into search and create fns

--- landsat_cogeo_mosaic/cli.py
+++ landsat_cogeo_mosaic/cli.py
@@ -5,7 +5,7 @@ from typing import List
 
 import click
 
-from .mosaic import create_mosaic
+from .stac import fetch_sat_api
 
 
 @click.group()
@@ -49,6 +49,66 @@ def main():
     show_default=True,
     help='Maximum date')
 @click.option(
+    '--season',
+    multiple=True,
+    default=None,
+    show_default=True,
+    type=click.Choice(["spring", "summer", "autumn", "winter"]),
+    help='Season, can provide multiple')
+@click.option(
+    '--stac-collection-limit',
+    type=int,
+    default=500,
+    show_default=True,
+    help='Limits the number of items per page returned by sat-api.')
+def search(
+        bounds, min_cloud, max_cloud, min_date, max_date, stac_collection_limit,
+        season):
+
+    bounds = tuple(map(float, re.split(r'[, ]+', bounds)))
+    if season is None:
+        season = ["spring", "summer", "autumn", "winter"]
+
+    start = datetime.strptime(min_date,
+                              "%Y-%m-%d").strftime("%Y-%m-%dT00:00:00Z")
+    end = datetime.strptime(max_date, "%Y-%m-%d").strftime("%Y-%m-%dT23:59:59Z")
+
+    query = {
+        "bbox": bounds,
+        "time": f"{start}/{end}",
+        "query": {
+            "eo:sun_elevation": {
+                "gt": 0
+            },
+            "landsat:tier": {
+                "eq": "T1"
+            },
+            "collection": {
+                "eq": "landsat-8-l1"
+            },
+            "eo:cloud_cover": {
+                "gte": min_cloud,
+                "lt": max_cloud
+            },
+            "eo:platform": {
+                "eq": "landsat-8"
+            },
+        },
+        "sort": [{
+            "field": "eo:cloud_cover",
+            "direction": "asc"
+        }],
+    }
+
+    if stac_collection_limit:
+        query['limit'] = stac_collection_limit
+
+    features = fetch_sat_api(query)
+    print(json.dumps(features, separators=(',', ':')))
+
+
+@click.command()
+@click.option(
     '--min-zoom',
     type=int,
     required=False,
@@ -69,44 +129,16 @@ def main():
     show_default=True,
     help='Limit one Path-Row scene per quadkey.')
 @click.option(
-    '--season',
-    multiple=True,
-    default=None,
-    show_default=True,
-    type=click.Choice(["spring", "summer", "autumn", "winter"]))
-@click.option(
     '--maximum-items-per-tile',
     type=int,
     default=20,
     show_default=True,
     help='Limit number of scene per quadkey. Use 0 to use all items.')
-@click.option(
-    '--stac-collection-limit',
-    type=int,
-    default=500,
-    show_default=True,
-    help='Limits the number of items per page returned by sat-api.')
-def create(
-        bounds, min_cloud, max_cloud, min_date, max_date, min_zoom, max_zoom,
-        optimized_selection, maximum_items_per_tile, stac_collection_limit,
-        season):
-
-    bounds = tuple(map(float, re.split(r'[, ]+', bounds)))
-    mosaic = create_mosaic(
-        bounds=bounds,
-        min_cloud=min_cloud,
-        max_cloud=max_cloud,
-        min_date=min_date,
-        max_date=max_date,
-        min_zoom=min_zoom,
-        max_zoom=max_zoom,
-        optimized_selection=optimized_selection,
-        maximum_items_per_tile=maximum_items_per_tile,
-        stac_collection_limit=stac_collection_limit,
-        seasons=season)
-    print(json.dumps(mosaic))
+def create(min_zoom, max_zoom, optimized_selection, maximum_items_per_tile):
+    pass
 
 
+main.add_command(search)
 main.add_command(create)
 
 if __name__ == '__main__':
