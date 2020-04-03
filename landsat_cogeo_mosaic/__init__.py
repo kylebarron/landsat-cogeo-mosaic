d1aaa6550b5bc1ae7b395684aa5fd830f5b54543
Kyle Barron <barronk@mit.edu>
2020-04-02 18:59:49 -0600

    Update package imports

--- landsat_cogeo_mosaic/__init__.py
+++ landsat_cogeo_mosaic/__init__.py
@@ -2,5 +2,5 @@ __author__ = """Kyle Barron"""
 __email__ = 'kylebarron2@gmail.com'
 __version__ = '0.1.0'
 
-from .mosaic import create_mosaic
-from .stac import stac_to_mosaicJSON
+from .mosaic import features_to_mosaicJSON
+from .stac import fetch_sat_api
