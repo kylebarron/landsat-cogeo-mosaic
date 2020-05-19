bb33b478951ff6288d5ad1bed9c9944fecf21d38
Kyle Barron <kylebarron2@gmail.com>
2020-05-19 00:49:25 -0600

    Refactor stac search into python API

--- landsat_cogeo_mosaic/cli.py
+++ landsat_cogeo_mosaic/cli.py
@@ -6,12 +6,11 @@ from datetime import datetime
 from pathlib import Path
 
 import click
-from dateutil.relativedelta import relativedelta
 
 from landsat_cogeo_mosaic.db import find_records
 from landsat_cogeo_mosaic.index import create_index
 from landsat_cogeo_mosaic.mosaic import StreamingParser, features_to_mosaicJSON
-from landsat_cogeo_mosaic.stac import fetch_sat_api
+from landsat_cogeo_mosaic.stac import search
 from landsat_cogeo_mosaic.util import filter_season
 from landsat_cogeo_mosaic.validate import missing_quadkeys as _missing_quadkeys
 from landsat_cogeo_mosaic.visualize import visualize as _visualize
@@ -93,63 +92,18 @@ def search(
         stac_collection_limit, season):
     """Retrieve features from sat-api
     """
-
-    bounds = tuple(map(float, re.split(r'[, ]+', bounds)))
-    start = datetime.strptime(min_date,
-                              "%Y-%m-%d").strftime("%Y-%m-%dT00:00:00Z")
-
-    if period:
-        start_datetime = datetime.strptime(start, "%Y-%m-%dT00:00:00Z")
-
-        if period == 'day':
-            delta = relativedelta(days=period_qty)
-        elif period == 'week':
-            delta = relativedelta(weeks=period_qty)
-        elif period == 'month':
-            delta = relativedelta(months=period_qty)
-        elif period == 'year':
-            delta = relativedelta(years=period_qty)
-
-        end_datetime = start_datetime + delta
-        end = end_datetime.strftime("%Y-%m-%dT00:00:00Z")
-    else:
-        end = datetime.strptime(max_date,
-                                "%Y-%m-%d").strftime("%Y-%m-%dT23:59:59Z")
-
-    query = {
-        "bbox": bounds,
-        "time": f"{start}/{end}",
-        "query": {
-            "eo:sun_elevation": {
-                "gt": 0
-            },
-            "landsat:tier": {
-                "eq": "T1"
-            },
-            "collection": {
-                "eq": "landsat-8-l1"
-            },
-            "eo:cloud_cover": {
-                "gte": min_cloud,
-                "lt": max_cloud
-            },
-            "eo:platform": {
-                "eq": "landsat-8"
-            },
-        },
-        "sort": [{
-            "field": "eo:cloud_cover",
-            "direction": "asc"
-        }],
-    }
-
-    if stac_collection_limit:
-        query['limit'] = stac_collection_limit
-
-    features = fetch_sat_api(query)
+    bounds = tuple(map(float, bounds.split(',')))
+    features = search(
+        bounds=bounds,
+        min_cloud=min_cloud,
+        max_cloud=max_cloud,
+        min_date=min_date,
+        max_date=max_date,
+        period=period,
+        period_qty=period_qty,
+        stac_collection_limit=stac_collection_limit)
     if not features:
-        print(
-            f"No asset found for query '{json.dumps(query)}'", file=sys.stderr)
+        print(f"No assets found for query", file=sys.stderr)
         return
 
     if season:
--- landsat_cogeo_mosaic/stac.py
+++ landsat_cogeo_mosaic/stac.py
@@ -30,8 +30,93 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import itertools
 import json
 import sys
+from datetime import datetime
+from typing import List
 
 import requests
+from dateutil.parser import parse as date_parse
+from dateutil.relativedelta import relativedelta
+
+
+def search(
+        bounds: List[float],
+        min_cloud: float = 0,
+        max_cloud: float = 100,
+        min_date='2013-01-01',
+        max_date=datetime.today(),
+        period: str = None,
+        period_qty: int = 1,
+        stac_collection_limit: int = 500,
+        stac_url: str = "https://sat-api.developmentseed.org"):
+    """Search STAC API given parameters
+
+    Args:
+        - bounds: minx, miny, maxx, maxy
+        - min_cloud: Minimum cloud percentage
+        - max_cloud: Maximum cloud percentage
+        - min_date: (str or datetime.datetime) Minimum date
+        - max_date: (str or datetime.datetime) Maximum date, inclusive
+        - period: Time period. If provided, overwrites `max-date` with the given period after `min-date`. One of 'day', 'week', 'month', 'year'
+        - period_qty: Number of periods to apply after `min-date`. Only applies if `period` is provided.
+        - stac_collection_limit: Limits the number of items per page returned by sat-api.
+        - stac_url: Endpoint to use. Defaults to Development Seed's Sat API
+    """
+
+    period_choices = ['day', 'week', 'month', 'year']
+    if period and period not in period_choices:
+        raise ValueError(f'period must be one of {period_choices}')
+
+    if not isinstance(min_date, datetime):
+        min_date = date_parse(min_date)
+    if not isinstance(max_date, datetime):
+        max_date = date_parse(max_date)
+
+    if period:
+        if period == 'day':
+            delta = relativedelta(days=period_qty)
+        elif period == 'week':
+            delta = relativedelta(weeks=period_qty)
+        elif period == 'month':
+            delta = relativedelta(months=period_qty)
+        elif period == 'year':
+            delta = relativedelta(years=period_qty)
+
+        max_date = min_date + delta
+
+    start = min_date.strftime("%Y-%m-%dT00:00:00Z")
+    end = max_date.strftime("%Y-%m-%dT23:59:59Z")
+
+    query = {
+        "bbox": bounds,
+        "time": f"{start}/{end}",
+        "query": {
+            "eo:sun_elevation": {
+                "gt": 0
+            },
+            "landsat:tier": {
+                "eq": "T1"
+            },
+            "collection": {
+                "eq": "landsat-8-l1"
+            },
+            "eo:cloud_cover": {
+                "gte": min_cloud,
+                "lt": max_cloud
+            },
+            "eo:platform": {
+                "eq": "landsat-8"
+            },
+        },
+        "sort": [{
+            "field": "eo:cloud_cover",
+            "direction": "asc"
+        }],
+    }
+
+    if stac_collection_limit:
+        query['limit'] = stac_collection_limit
+
+    return fetch_sat_api(query, stac_url=stac_url)
 
 
 def fetch_sat_api(query, stac_url: str = "https://sat-api.developmentseed.org"):
@@ -50,9 +135,9 @@ def fetch_sat_api(query, stac_url: str = "https://sat-api.developmentseed.org"):
     meta = data.get("meta", {})
     if not meta.get("found"):
         return []
-    else:
-        if meta['found'] >= 10000:
-            raise ValueError(f'Found {meta["found"]} results; max is 10,000')
+
+    if meta['found'] >= 10000:
+        raise ValueError(f'Found {meta["found"]} results; max is 10,000')
 
     print(json.dumps(meta), file=sys.stderr)
 
