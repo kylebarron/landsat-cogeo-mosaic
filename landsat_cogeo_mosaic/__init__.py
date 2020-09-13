3fbd125a6f93fddbd7c361ca3ea7bb7268a73386
Kyle Barron <kylebarron2@gmail.com>
2020-09-13 11:22:59 -0600

    Bump version: 0.1.1 → 0.2.0

--- landsat_cogeo_mosaic/__init__.py
+++ landsat_cogeo_mosaic/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Kyle Barron"""
 __email__ = 'kylebarron2@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 from .mosaic import features_to_mosaicJSON
 from .stac import fetch_sat_api
--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kylebarron/landsat-cogeo-mosaic',
-    version='0.1.1',
+    version='0.2.0',
     zip_safe=False,
 )
